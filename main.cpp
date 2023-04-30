2903c1ef77183f858a74601e4cc711f06f1cdd73
vance <iamvancenet@gmail.com>
2023-04-30 16:17:29 +0330

    Create the movePlayer function

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include "iostream"
 #include <conio.h>
-
+#include <unistd.h>
 using namespace std;
 
 class Maze_game
 {
 public:
-    enum eDirecton {LEFT, RIGHT, UP, DOWN};
-    eDirecton dir;
+    int x = 0;
+    int y = 1;
 
     char field [8][12] ={
         {'#','#','#','#','#','#','#','#','#','#','#','#'},
@@ -30,6 +30,80 @@ public:
                 cout << field[i][j];
             }
             cout << endl;
+        }
+    }
+
+    void movePlayer()
+    {
+        if (_kbhit())
+        {
+            switch (getch())
+            {
+                case 'a':
+                    if (field[y][x] == 'O')
+                    {
+                        x--;
+                        if (field[y][x] == ' ')
+                        {
+                            field[y][x] = 'O';
+                            x++;
+                            field[y][x] = ' ';
+                            x--; 
+                        }else if (field[y][x] == '#')
+                        {
+                            x++;
+                        }
+                    }
+                    break;
+                case 'd':
+                    if (field[y][x] == 'O')
+                    {
+                        x++;
+                        if (field[y][x] == ' ')
+                        {
+                            field[y][x] = 'O';
+                            x--;
+                            field[y][x] = ' ';
+                            x++;
+                        }else if (field[y][x] == '#')
+                        {
+                            x--;
+                        }
+                    }
+                    break;
+                case 'w':
+                    if (field[y][x] == 'O')
+                    {
+                        y--;
+                        if (field[y][x] == ' ')
+                        {
+                            field[y][x] = 'O';
+                            y++;
+                            field[y][x] = ' ';
+                            y--;
+                        }else if (field[y][x] = '#')
+                        {
+                            y++;
+                        }
+                    }
+                    break;
+                case 's':
+                    if (field[y][x] == 'O')
+                    {
+                        y++;
+                        if (field[y][x] == ' ')
+                        {
+                            field[y][x] = 'O';
+                            y--;
+                            field[y][x] = ' ';
+                            y++;
+                        }else if (field[y][x] = '#')
+                        {
+                            y--;
+                        }
+                    }
+                    break;
+            }
         }
     }
 };
